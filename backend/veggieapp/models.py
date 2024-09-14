51b19e3a990d9b6300ef37e30e798c499b9cbfc1
blairezh <113472440+blairezh@users.noreply.github.com>
2024-09-14 11:47:54 -0400

    models

--- backend/veggieapp/models.py
+++ backend/veggieapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+class Post(models.Model):
+    poster = models.CharField(max_length=200)
+    item = models.CharField(max_length=200)
+    item_description = models.CharField(max_length=200)
+    is_request = models.BooleanField
+    is_open = models.BooleanField
+    date_posted = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=200) #change to be functional
